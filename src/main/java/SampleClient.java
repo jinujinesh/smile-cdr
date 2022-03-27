de4b57eaecb386189f3e9c006e86d6efb642e8a8
Jinesh Thangavelu <jinujinesh@gmail.com>
2022-03-27 14:58:16 -0400

    Commit with Basic tasks

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -1,8 +1,15 @@
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
 
 public class SampleClient {
 
@@ -20,6 +27,23 @@ public class SampleClient {
                 .where(Patient.FAMILY.matches().value("SMITH"))
                 .returnBundle(Bundle.class)
                 .execute();
+            
+                List<BundleEntryComponent> bundleEntry  = response.getEntry();
+                List<Patient> patientsList = new ArrayList<Patient>();
+
+                bundleEntry.forEach(be -> patientsList.add((Patient)be.getResource()));
+
+
+                Comparator<Patient> patientNameComparator = ((p1, p2) -> p1.getName().get(0).getGiven().get(0).getValueAsString().compareTo(p2.getName().get(0).getGiven().get(0).getValueAsString()));
+                patientsList.sort(patientNameComparator);
+
+                // Basic tasks - listing of patients' first, last and dob & ordering by first name
+                patientsList.forEach(patient ->{
+                    System.out.println(patient.getName().get(0).getGiven().get(0).getValueAsString()
+                    + " | "+ patient.getName().get(0).getFamily()
+                    + " | "+ patient.getBirthDate());
+                });
+
 
 
     }
